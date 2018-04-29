69a8e5970d8ad37a865ac01546c860344251e5ec
VENKATA CHALLAGANDLA <atozprasad@MacBook-Pro-5.local>
2018-04-29 08:55:55 -0700

    Updated to fix the file issue and adding new route

---
+++ main.go
@@ -0,0 +1,199 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+	"io/ioutil"
+
+	"github.com/gin-gonic/gin"
+	"github.com/olivere/elastic"
+	"github.com/teris-io/shortid"
+)
+
+const (
+	elasticIndexName = "products"
+	elasticTypeName  = "product"
+)
+
+type Document struct {
+	ID        string    `json:"id"`
+	Name     string    `json:"name"`
+}
+
+type productcategory struct {
+Id      string  `json:"_id"`
+Name    string  `json:"name"`
+}
+type DocumentRequest struct {
+      Sku            int     `json:"sku"`
+      Name           string  `json:"name"`
+      Ptype          string  `json:"type"`
+      Price          float32 `json:"price"`
+      Upc            string     `json:"upc"`
+    Category       []productcategory
+      Description    string  `json:"description"`
+      Manufacturer   string  `json:"manufacturer"`
+      Model           string  `json:"model"`
+      Url             string    `json:"url"`
+      Image           string    `json:"image"`
+			Shipping       float32 `json:"shipping"`
+  }
+
+
+type DocumentResponse struct {
+	Name     string    `json:"name"`
+//	CreatedAt time.Time `json:"created_at"`
+}
+
+type SearchResponse struct {
+	Time      string             `json:"time"`
+	Hits      string             `json:"hits"`
+	Documents []DocumentResponse `json:"documents"`
+}
+
+var (
+	elasticClient *elastic.Client
+)
+var dataFilePath string="./products.json"
+
+func main() {
+	var err error
+
+	elasticURL:="http://localhost:9200"
+	// Create Elastic client and wait for Elasticsearch to be ready
+	for {
+		elasticClient, err = elastic.NewClient(
+			elastic.SetURL(elasticURL),
+			elastic.SetSniff(false),
+		)
+		if err != nil {
+			log.Println(err)
+			// Retry every 3 seconds
+			time.Sleep(3 * time.Second)
+		} else {
+			break
+		}
+	}
+	// Start HTTP server
+	r := gin.Default()
+	r.POST("/bulkupdload", bulkUploadEndpoint)
+	r.POST("/fileupload", createFromFileEndpoint)
+	r.GET("/search", searchEndpoint)
+
+	if err = r.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func bulkUploadEndpoint(c *gin.Context) {
+	// Parse request
+		var docs []DocumentRequest
+		if err := c.BindJSON(&docs); err != nil {
+			errorResponse(c, http.StatusBadRequest, "Malformed request body")
+			return
+		}
+		err:=AddProductsToIndex(docs,c,elasticClient)
+		 if err != nil {
+			errorResponse(c, http.StatusBadRequest, "Failed to post data in Elastic")
+ 			return
+		 }
+		 c.Status(http.StatusOK)
+	 }
+
+func createFromFileEndpoint(c *gin.Context) {
+		raw, err := ioutil.ReadFile(dataFilePath)
+	 if err != nil {
+		    errorResponse(c, http.StatusBadRequest, "Failed to read data from file. [Reason] " + err.Error())
+			 return
+	 }
+	 var docs []DocumentRequest
+	 err = json.Unmarshal(raw, &docs)
+	 if err != nil {
+		   errorResponse(c, http.StatusBadRequest, "Failed to convert data to json. [Reason] " + err.Error())
+			 return
+	 }
+
+		if err != nil {
+			 errorResponse(c, http.StatusBadRequest, "Failed to read data from file. [Reason] " + err.Error())
+			 return
+		}
+		err=AddProductsToIndex(docs,c,elasticClient)
+		 if err != nil {
+			errorResponse(c, http.StatusBadRequest, "Failed to post data in Elastic. [Reason] "+err.Error())
+			return
+		 }
+		c.Status(http.StatusOK)
+}
+func searchEndpoint(c *gin.Context) {
+	// Parse request
+		query := c.Query("query")
+		if query == "" {
+			errorResponse(c, http.StatusBadRequest, "Query not specified")
+			return
+		}
+		skip := 0
+		take := 10
+		if i, err := strconv.Atoi(c.Query("skip")); err == nil {
+			skip = i
+		}
+		if i, err := strconv.Atoi(c.Query("take")); err == nil {
+			take = i
+		}
+		// Perform search
+		esQuery := elastic.NewMultiMatchQuery(query, "name").
+			Fuzziness("2").
+			MinimumShouldMatch("2")
+		result, err := elasticClient.Search().
+			Index(elasticIndexName).
+			Query(esQuery).
+			From(skip).Size(take).
+			Do(c.Request.Context())
+		if err != nil {
+			log.Println(err)
+			errorResponse(c, http.StatusInternalServerError, "Something went wrong")
+			return
+		}
+		res := SearchResponse{
+			Time: fmt.Sprintf("%d", result.TookInMillis),
+			Hits: fmt.Sprintf("%d", result.Hits.TotalHits),
+		}
+		// Transform search results before returning them
+		docs := make([]DocumentResponse, 0)
+		for _, hit := range result.Hits.Hits {
+			var doc DocumentResponse
+			json.Unmarshal(*hit.Source, &doc)
+			docs = append(docs, doc)
+		}
+		res.Documents = docs
+		c.JSON(http.StatusOK, res)
+}
+
+func errorResponse(c *gin.Context, code int, err string) {
+		c.JSON(code, gin.H{
+			"error": err,
+		})
+}
+
+
+func AddProductsToIndex(docs []DocumentRequest,c *gin.Context, elasticClient *elastic.Client ) error {
+		bulk := elasticClient.
+		Bulk().
+		Index(elasticIndexName).
+		Type(elasticTypeName)
+		for _, d := range docs {
+			doc := Document{
+				ID:        shortid.MustGenerate(),
+				Name:     d.Name,
+				}
+			bulk.Add(elastic.NewBulkIndexRequest().Id(doc.ID).Doc(doc))
+		}
+		if _, err := bulk.Do(c.Request.Context()); err != nil {
+			errorResponse(c, http.StatusInternalServerError, "Failed to create documents  [Reason] " + err.Error())
+			return err
+		}
+		return nil
+}
